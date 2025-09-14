66f2fcd1e2c0b0b57d1c01f5f75cece10ab31092
Sanket Pathak <sanketpathak@192.168.1.6>
2025-09-14 12:09:15 +0530

    Added server dependency in api module to make existing get api working

--- api/src/main/java/com/reliaquest/api/ApiApplication.java
+++ api/src/main/java/com/reliaquest/api/ApiApplication.java
@@ -2,7 +2,9 @@ package com.reliaquest.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.reliaquest.server")
 @SpringBootApplication
 public class ApiApplication {
 
